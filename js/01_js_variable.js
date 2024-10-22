08cd8af64694c61277626e205aaa551c26b3d690
Shahzad Farooq <shahzadfarooqdev@gmail.com>
2024-10-22 12:08:05 +0500

    js:variables,web: flash sale done

---
+++ js/01_js_variable.js
@@ -0,0 +1,23 @@
+console.log("hello from the new file");
+// variables
+
+let myName = "Shahzad Farooq";
+let age = 21;
+console.log(age)
+age = 22;
+console.log(age)
+let MyName = "Ali Imran";
+// console.log(myName);
+// console.log(MyName);
+// console.log(age);
+
+
+const PI = 3.1415;
+console.log(PI)
+// PI = 4.22
+
+
+let num1 = 40;
+let num2 = 6;
+let result = num1 - num2;
+console.log(result)
