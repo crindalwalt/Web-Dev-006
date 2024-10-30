add7c4fbac6db4890c193ed4e50cb0f7314efc19
Shahzad Farooq <shahzadfarooqdev@gmail.com>
2024-10-30 11:06:52 +0500

    loops

---
+++ js/05_js_loops.js
@@ -0,0 +1,160 @@
+console.log("Loops");
+
+// Loop are repeatetive code running blocks
+// 3 types of looops (for, while , do while)
+/* loop require 3 perameters 
+        a variable
+        a condition
+        update to the variable
+*/
+
+// let friendsName = [1,2,3,4,5,6];
+let friendsName = [
+  "Muhtshim",
+  "Zaid",
+  "Shoaib",
+  "Husnain",
+  "Ahsan",
+  "Muzamil",
+  "Saqib",
+  "Nadeem",
+  "Ahmed",
+];
+// console.log(friendsName[0] + " kese ho bhai")
+// console.log(friendsName[1] + " kese ho bhai")
+// console.log(friendsName[2] + " kese ho bhai")
+// console.log(friendsName[3] + " kese ho bhai")
+// console.log(friendsName[4] + " kese ho bhai")
+// console.log(friendsName[5] + " kese ho bhai")
+// console.log(friendsName[6] + " kese ho bhai")
+// console.log(friendsName[7] + " kese ho bhai")
+// console.log(friendsName[8] + " kese ho bhai")
+
+// for(let index = 0; index < friendsName.length; index++){
+//     let element = friendsName[index];
+
+//     console.log(`Hello ${element} bhai! kese ho ap`);
+// }
+
+const pakistaniDishes = [
+  "Biryani",
+  "Karahi",
+  "Haleem",
+  "Nihari",
+  "Paya",
+  "Keema",
+  "Chapli Kebab",
+  "Seekh Kebab",
+  "Shami Kebab",
+  "Tandoori Chicken",
+  "Qeema Paratha",
+  "Aloo Paratha",
+  "Saag",
+  "Makki di Roti",
+  "Bhindi Masala",
+  "Daal Chawal",
+  "Chana Chaat",
+  "Gol Gappa",
+  "Pakora",
+  "Samosa",
+  "Kofta",
+  "Gola Kabab",
+  "Chappal Kabab",
+  "Mutton Korma",
+  "Chicken Handi",
+  "Beef Pulao",
+  "Mutton Pulao",
+  "Chicken Tikka",
+  "Fish Tikka",
+  "Aloo Gosht",
+  "Dum Ka Keema",
+  "Karhi Pakora",
+  "Murgh Musallam",
+  "Chicken Shashlik",
+  "Fried Fish",
+  "Prawn Masala",
+  "Sindhi Biryani",
+  "Hyderabadi Biryani",
+  "Lahori Chargha",
+  "Behari Kebab",
+  "Dhaka Fish",
+  "Masala Fish",
+  "Daal Maash",
+  "Daal Tarka",
+  "Bhuna Gosht",
+  "Balochi Sajji",
+  "Chicken Jalfrezi",
+  "Mutton Jalfrezi",
+  "Chili Chicken",
+  "Mughlai Handi",
+  "Chana Masala",
+  "Rajma",
+  "Roghni Naan",
+  "Kalonji Naan",
+  "Lachha Paratha",
+  "Chicken Korma",
+  "Mutton Korma",
+  "Peshawari Naan",
+  "Reshmi Kebab",
+  "Afghani Pulao",
+  "Bun Kebab",
+  "Aloo Tikki",
+  "Mughlai Paratha",
+  "Pakistani Pizza",
+  "Chinioti Kunna",
+  "Anday Wala Burger",
+  "Beef Haleem",
+  "Chicken Haleem",
+  "Siri Paye",
+  "Raita",
+  "Murgh Cholay",
+  "Bihari Boti",
+  "Chicken Manchurian",
+  "Gajrela",
+  "Shahi Tukray",
+  "Firni",
+  "Kheer",
+  "Rabri",
+  "Sheer Khurma",
+  "Zarda",
+  "Mutanjan",
+  "Jalebi",
+  "Gulab Jamun",
+  "Kofta Curry",
+  "Achar Gosht",
+  "Chicken Kebab",
+  "Shinwari Karahi",
+  "Katakat",
+  "Gajar Ka Halwa",
+  "Qorma",
+  "Karhi Chawal",
+  "Tikka Boti",
+  "Chicken Malai Boti",
+  "Bhuna Keema",
+  "Pathoray",
+  "Halwa Poori",
+  "Sindhi Kadhi",
+  "Chicken Makhani",
+  "Alu Keema",
+  "Mutton Masala",
+  "Pindi Chana",
+  "Tandoori Roti",
+  "Kulcha",
+];
+
+// let index = 0;
+// while (index < pakistaniDishes.length){
+
+//     let dish = pakistaniDishes[index];
+//     console.log(dish + " is pakistani dish")
+
+//     index++;
+// }
+
+// let index = 0;
+// do {
+//   let dish = pakistaniDishes[index];
+//   console.log(dish + " is pakistani dish");
+
+//   index++;
+// } while (index > pakistaniDishes.length);
